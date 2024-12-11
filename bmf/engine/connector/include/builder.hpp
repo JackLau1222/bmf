e623e04e84c8639f3afe408d81722cb1a1d38120
Jack Lau <2366536135@qq.com>
2024-12-11 22:49:44 +0800

    implement dynamic_add and update interface

--- bmf/engine/connector/include/builder.hpp
+++ bmf/engine/connector/include/builder.hpp
@@ -53,7 +53,7 @@ enum GraphMode {
     ServerMode,
     GeneratorMode,
     SubGraphMode,
-    UpdateMode,
+    PushDataMode,
 };
 enum InputManagerType { Immediate, Default, Server, FrameSync, ClockSync };
 enum ModuleType { CPP, C, Python, Go };
@@ -82,6 +82,8 @@ class RealStream : public std::enable_shared_from_this<RealStream> {
     void SetNotify(std::string const &notify);
 
     void SetAlias(std::string const &alias);
+
+    void SetIdentifier(std::string const &identifier);
     void Start();
 
     nlohmann::json Dump();
@@ -106,6 +108,7 @@ class RealStream : public std::enable_shared_from_this<RealStream> {
     std::string name_;
     std::string notify_;
     std::string alias_;
+    std::string identifier_;
 };
 
 class RealNode : public std::enable_shared_from_this<RealNode> {
@@ -236,9 +239,21 @@ class RealGraph : public std::enable_shared_from_this<RealGraph> {
                         bool dumpGraph, bool needMerge);
 
     int Run(bool dumpGraph, bool needMerge);
+    int Close();
     Packet Generate(std::string streamName, bool block = true);
     int FillPacket(std::string stream_name, Packet packet, bool block = false);
     std::shared_ptr<RealStream> InputStream(std::string streamName, std::string notify, std::string alias);
+
+    int generate_node_id();
+    int generate_add_id();
+
+    void DynamicRemove();
+    void DynamicAdd(std::shared_ptr<RealStream> module_stream, 
+                    std::shared_ptr<nlohmann::json> inputs, 
+                    std::shared_ptr<nlohmann::json> outputs);
+    void DynamicReset();
+    void Update(std::shared_ptr<RealGraph> update_graph);
+
   private:
     friend bmf::builder::Graph;
     friend bmf::builder::Node;
@@ -256,6 +271,10 @@ class RealGraph : public std::enable_shared_from_this<RealGraph> {
     std::shared_ptr<bmf::BMFGraph> graphInstance_ = nullptr;
     std::map<std::string, std::shared_ptr<RealStream>> existedStreamAlias_;
     std::map<std::string, std::shared_ptr<RealNode>> existedNodeAlias_;
+
+    std::mutex node_id_mutex;
+    int global_node_id_;
+    int global_add_id_;
 };
 } // namespace internal
 
@@ -636,6 +655,18 @@ class BMF_ENGINE_API Graph {
     void Start(bool dumpGraph = true, bool needMerge = true);
 
     void Start(std::vector<Stream>& generateStreams, bool dumpGraph = true, bool needMerge = true);
+
+    int Close();
+
+    void DynamicRemove();
+
+    void DynamicAdd(std::shared_ptr<Stream> module_stream, 
+                    std::shared_ptr<nlohmann::json> inputs, 
+                    std::shared_ptr<nlohmann::json> outputs);
+
+    void DynamicReset();
+
+    void Update(std::shared_ptr<Graph> update_graph);
 
     std::string Dump();
 
